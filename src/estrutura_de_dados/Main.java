3d8049514412f6dcb6e3e5917ea669162831c8aa
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-07-26 20:15:20 -0300

    Initial commit

---
+++ src/estrutura_de_dados/Main.java
@@ -0,0 +1,16 @@
+package estrutura_de_dados;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Estrutura de dados");
+
+        int[] lista = new int[2000];
+
+        lista[1999] = 16;
+        lista[10] = 10000;
+        lista[2000] = 7;
+
+    }
+}
