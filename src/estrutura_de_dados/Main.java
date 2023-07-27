dd088b03dcb493f8ef6914d5a2959c522f4c84ea
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-07-26 21:53:14 -0300

    .

--- src/estrutura_de_dados/Main.java
+++ src/estrutura_de_dados/Main.java
@@ -4,28 +4,65 @@ import java.util.ArrayList;
 
 public class Main {
 
+
+
     public static void main(String[] args) {
-
         System.out.println("Estrutura de dados");
-
-        int[] lista = new int[2000];
-
+        ItemListaEncadeada primeiro = null;
+        ItemListaEncadeada ultimo = null;
+        imprimeListaEncadeada(primeiro);
+        //1 primeiro elemento
+        ItemListaEncadeada item = new ItemListaEncadeada();
+        item.valor = 100;
+        item.proximo = null;
+        primeiro = item;
+        ultimo = item;
+        imprimeListaEncadeada(primeiro);
+        //segundo elemento
+        item = new ItemListaEncadeada();
+        item.valor = 85;
+        item.proximo = null;
+        ultimo.proximo = item;
+        ultimo = item;
+        imprimeListaEncadeada(primeiro);
+        /*
+        int[] lista = new int[0];
         lista = add(lista, 16);
         lista = add(lista, 1000);
 
         ArrayList lista2 = new ArrayList();
-        lista2.add(15);
+        lista2.add(16);
         lista2.add(1000);
+        */
 
     }
 
-    private static int[] add(int[] lista, int novoValor) {
-        int[] novaLista = new int[lista.length];
+    public static void imprimeListaEncadeada(
+            ItemListaEncadeada primeiro) {
+        ItemListaEncadeada proximo = primeiro;
+        System.out.println("Imprimindo lista...");
+        while(proximo != null) {
+            System.out.println(proximo.valor);
+            proximo = proximo.proximo;
+        }
+    }
 
-        for (int i = 0; i < lista.length; i++) {
+
+    public static class ItemListaEncadeada{
+        int valor;
+        ItemListaEncadeada proximo;
+    }
+
+
+    public static int[] add(int[] lista, int novoValor) {
+        int[] novaLista = new int[lista.length+1];
+        for(int i=0; i< lista.length; i++) {
             novaLista[i] = lista[i];
         }
         novaLista[lista.length] = novoValor;
         return novaLista;
     }
+
+
+
 }
