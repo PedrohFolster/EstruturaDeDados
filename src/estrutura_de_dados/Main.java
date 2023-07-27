8804fc52604aec177262461c9e647f31d3b7e356
pedro.santos13 <pedro.santos13@alunos.sc.senac.br>
2023-07-26 21:14:35 -0300

    .

--- src/estrutura_de_dados/Main.java
+++ src/estrutura_de_dados/Main.java
@@ -1,5 +1,7 @@
 package estrutura_de_dados;
 
+import java.util.ArrayList;
+
 public class Main {
 
     public static void main(String[] args) {
@@ -8,9 +10,22 @@ public class Main {
 
         int[] lista = new int[2000];
 
-        lista[1999] = 16;
-        lista[10] = 10000;
-        lista[2000] = 7;
+        lista = add(lista, 16);
+        lista = add(lista, 1000);
 
+        ArrayList lista2 = new ArrayList();
+        lista2.add(15);
+        lista2.add(1000);
+
+    }
+
+    private static int[] add(int[] lista, int novoValor) {
+        int[] novaLista = new int[lista.length];
+
+        for (int i = 0; i < lista.length; i++) {
+            novaLista[i] = lista[i];
+        }
+        novaLista[lista.length] = novoValor;
+        return novaLista;
     }
 }
